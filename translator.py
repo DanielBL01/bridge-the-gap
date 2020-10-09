c6073149ae1a1b427e3a117f60c9f7d3da5ba42b
Daniel Lee <dblee0532@gmail.com>
2020-10-09 03:34:13 -0400

    Added translation function

--- translator.py
+++ translator.py
@@ -3,11 +3,19 @@ from google.cloud import translate_v2 as translate
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/home/daniel/workspace/python/live-chat-translator/creds/google_creds.json'
 
-translate_client = translate.Client()
+def translate(text, target, translate_client = translate.Client()):
+	'''
+	Google Cloud Translation API will take text and the desired
+	translated language which Google has a list of supported languages
+	here: https://cloud.google.com/translate/docs/languages 
 
-text = 'Hello, my name is Daniel'
-target = 'ko'
+	:return: dict of results containing input, translatedText, and
+			 detectedSourceLanguage
+	'''
 
-result = translate_client.translate(text, target_language=target)
+	result = translate_client.translate(text, target_language=target)
 
-print(result)
+	return result['translatedText']
+
+print(translate('내 이름은 다니엘이야', 'en') + '\n')
+print(translate('My name is Daniel', 'ko'))
