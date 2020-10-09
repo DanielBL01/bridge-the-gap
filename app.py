5a8103f77c67434d64db05fea9dd7773742b414a
Daniel Lee <dblee0532@gmail.com>
2020-10-09 07:14:35 -0400

    Added sessions for login and appropriate message flashing

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, flash, render_template, redirect, url_for
+from flask_login import LoginManager, login_user, logout_user, current_user, login_required
 
 from wtform import *
 from models import *
@@ -10,6 +11,13 @@ app.secret_key = 'SECRET KEY'
 # Configure database hosted by Heroku
 app.config['SQLALCHEMY_DATABASE_URI'] = 'SECRET HEROKU DATABASE URI'
 db = SQLAlchemy(app)
+
+login_manager = LoginManager(app)
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+	return User.query.get(int(user_id))
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
@@ -33,6 +41,7 @@ def index():
 		db.session.add(user)
 		db.session.commit()
 
+		flash('Registered successfully. Please log in.', 'success')
 		return redirect(url_for('login'))
 		
 	return render_template("index.html", my_form = registration_form)
@@ -44,10 +53,28 @@ def login():
 	login_form = LoginForm()
 
 	if login_form.validate_on_submit():
-		return "Welcome to Bridge the Gap!"
+		user_object = User.query.filter_by(username=login_form.username.data).first()
+		login_user(user_object)
+		
+		return redirect(url_for('chat'))
 
 	# Handling case if user uses GET method to visit login page
 	return render_template("login.html", my_form=login_form)
+
+@app.route("/chat", methods=['GET', 'POST'])
+def chat():
+	if not current_user.is_authenticated:
+		flash('Please login.', 'danger') 
+		return redirect(url_for('login'))
+
+	return "Welcome to Bridge the Gap chat room!"
+
+@app.route("/logout", methods=['GET'])
+def logout():
+	
+	logout_user()
+	flash('You have sucessfully logged out', 'sucess')
+	return redirect(url_for('login'))
 
 if __name__ == "__main__":
 	app.run(debug=True)
